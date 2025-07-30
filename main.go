d001db9f279946be56c2c1841823017da1dca120
jadedragon942 <djade942@gmail.com>
2025-07-30 15:43:18 +0000

    f

--- main.go
+++ main.go
@@ -72,7 +72,9 @@ func (bot *CinemaBot) loadConfig(configFile string) error {
 		// Default config if no file specified
 		bot.config = Config{
 			Server:       "irc.snoonet.org:6667",
-			Nick:         "marquee",
+			Nick:         "marquee
+			
+			",
 			Channel:      "#stopdrinkingcinema",
 			DatabasePath: "cinema_bot.db",
 		}
